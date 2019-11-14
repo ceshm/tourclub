453aaf539c37feaf34a472b03619f24d75162236
ceshm <contact@ceshm.com>
2019-11-13 19:05:53 -0600

    base requirements and tour models

---
+++ models.py
@@ -0,0 +1,25 @@
+import json
+
+from tortoise.models import Model
+from tortoise import fields
+
+
+with open('config.json') as config_file:
+    db_config = json.load(config_file)["database"]
+
+
+class Tour(Model):
+    id = fields.IntField(pk=True)
+    name = fields.TextField()
+    data = fields.JSONField()
+
+    def __str__(self):
+        return self.name
+
+
+class Team(Model):
+    id = fields.IntField(pk=True)
+    name = fields.TextField()
+
+    def __str__(self):
+        return self.name
